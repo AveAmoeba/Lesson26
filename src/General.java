1c43ad6ebd02ff23dde80b8bcc725707d11b193a
Сэр Рыцарь <StrateSkate@gmail.com>
2022-07-05 20:10:32 +0300

    Слава великой Амёбе

--- src/General.java
+++ src/General.java
@@ -2,6 +2,7 @@ public class General {
     public static void main(String[] args) {
         System.out.println(sum(1,8));
         System.out.println(mult(1,8));
+        System.out.println("Амёмбиус");
     }
 
     private static int sum(int x, int y) {
