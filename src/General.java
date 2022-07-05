1c68ea0b3f1f49d6245bd4f248406d92af7b0a58
IHOLY <LESHA744@GMAIL.COM>
2022-07-05 20:01:22 +0300

    Добавлена функция умножения Теперь есть возможность испепелять взглядом нажатие выполнение функции дает 200 тыщ долларов

--- src/General.java
+++ src/General.java
@@ -1,9 +1,14 @@
 public class General {
     public static void main(String[] args) {
         System.out.println(sum(1,8));
+        System.out.println(mult(1,8));
     }
 
     private static int sum(int x, int y) {
         return x + y;
+    }
+
+    private static int mult(int x, int y) {
+        return x * y;
     }
 }
